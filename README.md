71b8df0901d9e5924059a2f4460be55e084f01a8
odoe <odoe@odoe.net>
2014-06-05 12:10:55 -0700

    simpleedit module now working, updated README

--- README.md
+++ README.md
@@ -5,6 +5,34 @@ This is a simple edit widget for [ArcGIS JavaScript](http://developers.arcgis.co
 
 The idea behind this widget is to wrap very simple edit tasks in a widget to quickly add editing capabilities to a map project.
 
+Configuration
+---
+
+```javascript
+{
+  "name": "simpleedit",
+  "path": "widgets/simpleedit/simpleedit",
+  "options": {
+    "useDialog": true,
+    "duration": 1000,
+    "editLayers": [
+      {
+        "layerId": "myLayerId",
+        "editType": "EDIT_VERTICES"
+      }
+    ]
+  }
+}
+```
+
+If `useDialog` is omitted or false, widget will use native JavaScript
+[confirm](https://developer.mozilla.org/en-US/docs/Web/API/Window.confirm) to
+verfiy delete. If not, it will use a [dijit/Dialog](http://dojotoolkit.org/reference-guide/1.9/dijit/Dialog.html) to confirm deleting of features.
+
+Double-click a feature in specified layer to start editing.
+
+You can delete a feature by holding down the mouse on a feature while it is
+being edited for specified `duration`. Default is 1000.
 
 *A work in progress*
 
---
+++ simpleedit.js
@@ -0,0 +1,208 @@
+/*global define, clearTimeout, setTimeout*/
+define([
+  'dojo/_base/declare',
+  'dojo/_base/lang',
+  'dojo/_base/array',
+  'dojo/Evented',
+  'dojo/on',
+
+  'dijit/_WidgetBase',
+
+  'dijit/Dialog',
+
+  'esri/toolbars/edit'
+], function(
+  declare, lang, arrayUtils,
+  Evented, on,
+  _WidgetBase,
+  Dialog,
+  Edit
+) {
+  'use strict';
+
+  function getLayerId(lyr) {
+    return lyr.layerId;
+  }
+
+  function getLayer(graphic) {
+    return graphic._graphicsLayer;
+  }
+
+  function head(t) {
+    return t[0];
+  }
+
+  return declare([_WidgetBase, Evented], {
+
+    // will be used to determine if a layer is
+    // currently being edited
+    editAction: {},
+
+    startup: function() {
+      if (!this.get('map')) {
+        this.destroy();
+        console.error('SimpleEdit::map required');
+      }
+
+      if (!this.get('duration')) {
+        this.set('duration', 1000);
+      }
+
+      if (this.get('useDialog')) {
+        this.verifyDialog = new Dialog({
+          title: 'Confirm Delete',
+          closable: false,
+          content: [
+            '<div><button class="btn btn-danger simple-delete">',
+            'Yes</button>',
+            '&nbsp;&nbsp;',
+            '<button class="btn btn-success simple-cancel">',
+            'No</button></div>'
+          ].join('')
+        });
+
+        var node = this.verifyDialog.domNode;
+        this.own(
+          on(node, '.simple-delete:click', lang.hitch(this, 'deleteFeature')),
+          on(node, '.simple-cancel:click', lang.hitch(this, function() {
+            this.verifyDialog.hide();
+          }))
+        );
+      }
+
+      this.set('editTool', new Edit(this.get('map')));
+
+      if (this.get('map').loaded) {
+        this.setup();
+      } else {
+        on.once(this.get('map'), 'load', lang.hitch(this, 'setup'));
+      }
+
+    },
+
+    setup: function() {
+      var hasLayerIds = arrayUtils.filter(this.get('editLayers'), getLayerId);
+      // listen for Edit toolbar deactivate
+      this.own(
+        on(this.get('editTool'), 'deactivate', lang.hitch(this, 'applyEdits'))
+      );
+      // iterate editable layers and listen for dbl-click
+      this.set('editableLayers', arrayUtils.map(hasLayerIds, function(data) {
+        var layer = this.get('map').getLayer(data.layerId);
+        this.editAction[data.layerId] = {
+          editing: false,
+          editType: data.editType || 'EDIT_VERTICES' // default to EDIT_VERTICES
+        };
+        this.own(
+          on(layer, 'dbl-click', lang.hitch(this, 'onLayerDblClick')),
+          on(layer, 'mouse-down', lang.hitch(this, 'handleMouseDown')),
+          on(layer, 'mouse-up', lang.hitch(this, 'handleMouseUp'))
+        );
+        return layer;
+      }, this));
+
+      this._init();
+    },
+
+    // helper method to find editable layers by id
+    findEditLayer: function(id) {
+      return head(arrayUtils.filter(this.get('editableLayers'), function(layer) {
+        return layer.id === id;
+      }));
+    },
+
+    deactivateEdit: function(layerId) {
+      this.get('editTool').deactivate();
+      this.editAction[layerId].editing = false;
+      this.emit('edit-tool-deactivate', {});
+    },
+
+    // apply edits when done
+    applyEdits: function(e) {
+      // if feature gets deleted it no longer belongs
+      // to a graphicslayer, so check first
+      if (e.graphic._graphicsLayer && e.info.isModified) {
+        var layer = this.findEditLayer(getLayer(e.graphic).id);
+        layer.applyEdits(
+          null, [e.graphic], null
+        ).then(lang.hitch(this, function() {
+          this.emit('edit-tool-edits-applied-complete', arguments);
+        }), lang.hitch(this, function() {
+          this.emit('edit-tool-edits-applied-error', arguments);
+        }));
+      }
+    },
+
+    // edit vertices on dbl-click
+    onLayerDblClick: function(e) {
+      e.preventDefault();
+      var layer, editing, editType;
+      layer = this.findEditLayer(getLayer(e.graphic).id);
+      editing = this.editAction[layer.id].editing;
+      if (!editing) {
+        editType = this.editAction[layer.id].editType;
+        this.editAction[layer.id].editing = true;
+        this.get('editTool').activate(Edit[editType], e.graphic);
+        this.emit('edit-tool-activate', e.graphic);
+      } else {
+        this.deactivateEdit(layer.id);
+      }
+    },
+
+    deleteFeature: function(feature) {
+      var graphic;
+      if (this.get('useDialog')) {
+        graphic = this.verifyDialog._graphic;
+      } else {
+        graphic = feature;
+      }
+      if (graphic) {
+        var layer = this.findEditLayer(getLayer(graphic).id);
+        this.deactivateEdit(layer.id);
+        layer.applyEdits(
+          null, null, [graphic]
+        ).then(lang.hitch(this, function() {
+          if (this.get('useDialog')) {
+            this.verifyDialog._graphic = null;
+            this.verifyDialog.hide();
+            this.emit('edit-tool-edits-applied-complete', arguments);
+          }
+        }), lang.hitch(this, function() {
+          this.emit('edit-tool-edits-applied-error', arguments);
+        }));
+      }
+    },
+
+    handleMouseDown: function(e) {
+      e.preventDefault();
+      var layer, editing;
+      layer = this.findEditLayer(getLayer(e.graphic).id);
+      editing = this.editAction[layer.id].editing;
+      if (editing) {
+        e.stopPropagation();
+        this.timeoutID = setTimeout(lang.hitch(this, function() {
+          if (this.get('useDialog')) {
+            this.verifyDialog._graphic = e.graphic;
+            this.verifyDialog.show();
+          } else if (confirm('Confirm Delete')) { // use native confirm
+            this.deleteFeature(e.graphic);
+          }
+        }), this.get('duration'));
+      }
+    },
+
+    handleMouseUp: function(e) {
+      if(typeof this.timeoutID == "number") {
+        clearTimeout(this.timeoutID);
+        delete this.timeoutID;
+      }
+    },
+
+    _init: function() {
+      this.set('loaded', true);
+      this.emit('loaded', {});
+    }
+
+  });
+
+});
